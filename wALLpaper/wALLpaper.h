5c744d50274e3d285eaf9272b8b267427740af5e
Matthew Dahl <mdahl@mintrus.com>
2013-03-03 10:07:56 -0500

    Initial Commit

---
+++ wALLpaper/wALLpaper.h
@@ -0,0 +1,15 @@
+//
+//  wALLpaper.h
+//  wALLpaper
+//
+//  Created by Matthew Dahl on 3/3/13.
+//  Copyright (c) 2013 Mintrus, LLC. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface wALLpaper : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ wALLpaper/wALLpaper.m
@@ -0,0 +1,17 @@
+//
+//  wALLpaper.m
+//  wALLpaper
+//
+//  Created by Matthew Dahl on 3/3/13.
+//  Copyright (c) 2013 Mintrus, LLC. All rights reserved.
+//
+
+#import "wALLpaper.h"
+
+@implementation wALLpaper
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
